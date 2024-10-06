6fd7981e3162591e68ab1012d1b8fb08cfc5f280
Melvyn Nyakoe Nyasani <157981663+romelvinyo99@users.noreply.github.com>
2024-10-07 00:24:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def main():
     options = option_menu(
         menu_title=None,
         options=["Home Page", "Preprocessing", "Clustering", "Visualizations", "Prediction"],
-        icons=["house", "gear-fill", "collection-fill", "pie-chart-fill", "arrow-repeat"],
+        icons=["house", "hammer", "collection-fill", "pie-chart-fill", "gear-fill"],
         orientation="horizontal"
     )
     # Global variables
