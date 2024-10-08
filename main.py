ce7942ca2ee52348e75141d4ff7aeb7b2ffc85b8
Melvyn Nyakoe Nyasani <157981663+romelvinyo99@users.noreply.github.com>
2024-10-08 10:52:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -28,8 +28,8 @@ def main():
     # Options menu
     options = option_menu(
         menu_title=None,
-        options=["Homepage", "Preprocessing", "Clusterings", "Visualizations", "Predictions"],
-        icons=["house    ", "hammer", "collection-fill", "pie-chart-fill", "gear-fill"],
+        options=["Home Page", "Preprocessing", "Clustering", "Visualizations", "Prediction"],
+        icons=["house", "gear-wide-connect", "collection-fill", "house", "arrow-repeat"],
         orientation="horizontal"
     )
     # Global variables
@@ -42,11 +42,13 @@ def main():
         st.session_state.labeller = None
     if "model" not in st.session_state:
         st.session_state.model = None
+    if "optimal" not in st.session_state:
+        st.session_state.optimal = None
 
     # Pages --> Instantiations
     if raw_data:
         df = pd.read_csv(raw_data)
-        if options == "Homepage":
+        if options == "Home Page":
             home = Home(df)
             home.datasetOverview()
 
@@ -56,20 +58,21 @@ def main():
             st.session_state.scaler = scale
             st.session_state.labeller = label
             st.session_state.preprocessed_data = data
-        if options == "Clusterings":
+        if options == "Clustering":
             if st.session_state.labeller is not None and st.session_state.scaler is not None and st.session_state.preprocessed_data is not None:
                 model = Clustering(st.session_state.preprocessed_data)
-                m = model.text()
+                m, k = model.text()
                 st.session_state.model = m
+                st.session_state.optimal = k
             else:
                 st.warning("Complete Preprocessing")
         if options == "Visualizations":
             visual = Plotting(st.session_state.preprocessed_data)
             visual.pairplot()
-        if options == "Predictions":
+        if options == "Prediction":
             if st.session_state.model is not None:
                 model_instance = Prediction(st.session_state.preprocessed_data, st.session_state.scaler,
-                                            st.session_state.labeller, st.session_state.model)
+                                            st.session_state.labeller, st.session_state.model, st.session_state.optimal)
                 model_instance.cluster_predict()
             else:
                 st.warning("Complete clustering")
